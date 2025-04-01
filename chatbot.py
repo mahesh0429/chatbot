eb309ba5a7c8cb3a7d1b8a5c6ba44c2bbc75194d
KANCHARLA MAHESH KUMAR NAIDU <maheshkumarkancharla2040@gmail.com>
2025-04-01 10:53:18 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,46 @@
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+
+intents = {
+    'greeting': ['hello', 'hi', 'hey'],
+    'goodbye': ['bye', 'see you later'],
+    'thanks': ['thank you', 'thanks'],
+    'help': ['help', 'what can you do'],
+    'name': ['what is your name?','who are you?'],
+    'love':['you have an girl friend?']
+}
+
+
+responses = {
+    'greeting': 'Hello! How can I assist you today?',
+    'goodbye': 'See you later! Have a great day!',
+    'thanks': 'You\'re welcome!',
+    'help': 'I can answer your questions, provide information, and assist with tasks.',
+    'name': 'My Name is Mahi Chatbot,nice to meet you',
+    'love': 'yes,her name is chinni'
+}
+
+def chatbot(message):
+                          
+    tokens = nltk.word_tokenize(message)
+    
+                          
+    lemmas = [lemmatizer.lemmatize(token) for token in tokens]
+    
+                       
+    for intent, keywords in intents.items():
+        for keyword in keywords:
+            if keyword in message.lower():
+                return responses[intent]
+    
+                      
+    return 'I didn\'t understand that. Can you please rephrase?'
+
+
+while True:
+    message = input('User: ')
+    response = chatbot(message)
+    print('Mahi Chatbot:', response)
